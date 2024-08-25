e5712b05c209767c9506633eb609ad5632646330
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2024-08-25 22:51:03 +0700

    feat: update responsive

--- src/routes/Client/Footware/Slipon.jsx
+++ src/routes/Client/Footware/Slipon.jsx
@@ -4,6 +4,7 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { motion } from 'framer-motion';
 
 const Slipon = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -13,6 +14,7 @@ const Slipon = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [selectedBrands, setSelectedBrands] = useState([]);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const fetchProducts = async (token) => {
     try {
@@ -119,18 +121,27 @@ const Slipon = () => {
     return selectedBrands.some((brand) => productName.toLowerCase().includes(brand.toLowerCase()));
   };
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const sidebarVariants = {
+    open: { x: 0, opacity: 1, transition: { duration: 0.2, ease: 'easeInOut' } },
+    closed: { x: '-100%', opacity: 0, transition: { duration: 0.2, ease: 'easeInOut' } },
+  };
+
   return (
     <>
-      <div className="mt-20 max-w-[115rem] py-5 mx-auto px-5 md:px-2 flex flex-col justify-center items-center">
-        <img src="/hero-slipon.png" alt="Hero Slipon" className="w-full h-auto mb-6" />
+      <div className="mt-20 max-w-[115rem] py-5 mx-auto px-5 md:px-2 flex flex-col justify-center items-center overflow-x-hidden">
+        <img src="/hero-slipon.png" alt="Hero Slipon" className="w-full h-full md:h-auto mb-6" />
         {/* Filter  */}
-        <div className="w-full flex justify-between mb-6 sticky top-[72px] bg-white z-[997] py-4">
+        <div className="w-full flex justify-between mb-6 sticky top-[70px] bg-white z-[997] py-1 md:py-4">
           <div className="flex flex-grow">
-            <div className="border border-[#E5E5E5] flex items-center justify-center w-[17rem] px-10 py-5 gap-x-14">
-              <p className="font-overpass text-lg">Filter</p>
-              <svg width="24" height="24" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div className="border border-[#E5E5E5] flex items-center justify-center w-[10rem] md:w-[17rem] px-4 md:px-10 py-5 gap-x-5 md:gap-x-14">
+              <p className="font-overpass text-lg hidden md:block">Filter</p>
+              <svg width="24" height="24" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleSidebar}>
                 <path d="M18.3335 5.41666H13.3335" stroke="#292D32" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M4.1871084 5.41666H1.6665" stroke="#292D32" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M4.99984 5.41666H1.6665" stroke="#292D32" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                 <path
                   d="M8.33317 8.33333C9.944 8.33333 11.2498 7.0275 11.2498 5.41667C11.2498 3.80584 9.944 2.5 8.33317 2.5C6.72234 2.5 5.4165 3.80584 5.4165 5.41667C5.4165 7.0275 6.72234 8.33333 8.33317 8.33333Z"
                   stroke="#292D32"
@@ -151,10 +162,10 @@ const Slipon = () => {
                 />
               </svg>
             </div>
-            <div className="border-t border-b border-[#E5E5E5] flex-grow flex items-center px-10 py-5">
+            <div className="border-t border-b border-r lg:border-r-0 border-[#E5E5E5] flex-grow flex items-center px-4 md:px-10 py-5">
               <p className="font-overpass capitalize">{products.filter((item) => [1027, 18710].includes(item.item_category_id)).flatMap((item) => item.variants).length} Hasil</p>
             </div>
-            <div className="relative border border-[#E5E5E5] flex items-center justify-center w-[25rem] px-10 py-5 gap-x-5">
+            <div className="relative border border-[#E5E5E5] hidden md:flex items-center justify-center w-full md:w-[25rem] px-4 md:px-10 py-5 gap-x-5">
               <p className="font-overpass capitalize cursor-pointer" onClick={handleDropdownToggle}>
                 {selectedOption}
               </p>
@@ -180,9 +191,9 @@ const Slipon = () => {
             </div>
           </div>
         </div>
-        <div className="w-full flex justify-between gap-x-3">
+        <div className="w-full flex justify-between md:gap-x-3 overflow-x-hidden">
           {/* Sidebar Filter */}
-          <div className="w-[15%] border border-[#E5E5E5] flex flex-col px-6 py-6 h-[calc(100vh-4rem)] overflow-y-auto">
+          <div className="w-[15%] border border-[#E5E5E5] flex-col px-6 py-6 h-[calc(100vh-4rem)] overflow-y-auto hidden md:flex md:py-5">
             {/* Filter Brand */}
             <div className="mb-6">
               <h3 className="text-lg font-medium font-overpass">Brand</h3>
@@ -214,15 +225,60 @@ const Slipon = () => {
               </ul>
             </div>
           </div>
+
+          {isSidebarOpen && (
+            <motion.div
+              className="fixed inset-0 bg-white z-[999] flex flex-col w-3/4 h-full px-6 py-6 overflow-y-auto md:hidden overflow-x-hidden"
+              initial="closed"
+              animate={isSidebarOpen ? 'open' : 'closed'}
+              variants={sidebarVariants}
+              transition={{ type: 'spring', stiffness: 100 }}
+            >
+              <button onClick={toggleSidebar} className="self-end text-xl font-bold mb-4">
+                ×
+              </button>
+              {/* Filter Brand */}
+              <div className="mb-6">
+                <h3 className="text-lg font-medium font-overpass">Brand</h3>
+                <ul className="mt-3 space-y-2">
+                  <li className="flex items-center gap-x-2">
+                    <input type="checkbox" className="border border-[#E5E5E5] focus:outline-none focus:shadow-outline focus:border-[#E5E5E5] focus:ring-0" name="brand" id="JORDAN" value="JORDAN" onChange={handleBrandChange} />
+                    <label className="font-overpass" htmlFor="JORDAN">
+                      JORDAN
+                    </label>
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <input type="checkbox" className="border border-[#E5E5E5] focus:outline-none focus:shadow-outline focus:border-[#E5E5E5] focus:ring-0" name="brand" id="OFFWHITE" value="OFFWHITE" onChange={handleBrandChange} />
+                    <label className="font-overpass" htmlFor="OFFWHITE">
+                      OFFWHITE
+                    </label>
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <input type="checkbox" className="border border-[#E5E5E5] focus:outline-none focus:shadow-outline focus:border-[#E5E5E5] focus:ring-0" name="brand" id="BAPE" value="BAPE" onChange={handleBrandChange} />
+                    <label className="font-overpass" htmlFor="BAPE">
+                      BAPE
+                    </label>
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <input type="checkbox" className="border border-[#E5E5E5] focus:outline-none focus:shadow-outline focus:border-[#E5E5E5] focus:ring-0" name="brand" id="OFFWHITE" value="OFFWHITE" onChange={handleBrandChange} />
+                    <label className="font-overpass" htmlFor="OFFWHITE">
+                      OFFWHITE
+                    </label>
+                  </li>
+                </ul>
+              </div>
+            </motion.div>
+          )}
+
           {/* Product */}
-          <div className="w-[85%] flex flex-col gap-y-8 md:flex-row md:flex-wrap md:justify-between mb-10 overflow-y-auto h-[calc(100vh-4rem)] px-5">
+          <div className="w-full md:w-[85%] grid grid-cols-2 gap-5 md:grid-cols-3 mb-10 overflow-y-auto h-[calc(100vh-4rem)] md:px-5 overflow-x-hidden">
             {isLoading ? (
               Array.from({ length: 9 }).map((_, index) => (
                 <div key={index} className="flex flex-col gap-y-5 items-center">
-                  <Skeleton className="w-[30rem] h-[30rem]" />
-                  <div className="flex flex-col text-center gap-y-2">
-                    <Skeleton className="text-xl md:w-[24rem]" />
-                    <Skeleton className="text-xl" />
+                  <Skeleton className="w-[10rem] h-[10rem] mobileS:w-[10.5rem] mobileS:h-[10.5rem] mobile:w-[11.5rem] mobile:h-[11.5rem] md:w-[23rem] md:h-[23rem] lg:w-[31rem] lg:h-[31rem] laptopL:w-[27rem] laptopL:h-[27rem] object-cover" />
+                  <div className="flex flex-col text-center gap-y-2 w-full">
+                    <Skeleton className=" md:text-xl w-[10rem] mobileS:w-[10.5rem] mobile:w-[11.5rem] md:w-[24rem]" />
+                    <Skeleton className="md:text-xl" />
                   </div>
                 </div>
               ))
@@ -281,16 +337,22 @@ const Slipon = () => {
                   .filter((variant) => variant.sell_price !== null && variant.sell_price !== 0 && (variant.available_qty === null || variant.available_qty <= 1))
                   .map((variant, index) => (
                     <div key={index} className="flex flex-col gap-y-5 items-center">
-                      <Link to="/">{variant.parentThumbnail ? <img src={variant.parentThumbnail} alt={variant.item_name} className="w-[30rem]" /> : <img src="/dummy-product.png" alt={variant.item_name} className="w-[30rem]" />}</Link>
+                      <Link to={`/product-detail/${variant.item_id}`}>
+                        {variant.parentThumbnail ? (
+                          <img src={variant.parentThumbnail} alt={variant.item_name} className="w-[10rem] mobileS:w-[10.5rem] mobile:w-[11.5rem] md:w-[23rem] lg:w-[31rem] laptopL:w-[27rem] object-cover" />
+                        ) : (
+                          <img src="/dummy-product.png" alt={variant.item_name} className="w-[10rem] mobileS:w-[10.5rem] mobile:w-[11.5rem] md:w-[23rem] lg:w-[31rem] laptopL:w-[27rem] object-cover" />
+                        )}
+                      </Link>
                       <div className="flex flex-col text-center gap-y-2">
-                        <h2 className="uppercase font-overpass font-extrabold text-xl md:w-[24rem] text-red-600">{variant.item_name}</h2>
-                        <h2 className="uppercase font-overpass text-xl text-red-600">Sold Out</h2>
+                        <h2 className="uppercase font-overpass font-extrabold  md:text-xl w-[10rem] mobileS:w-[10.5rem] mobile:w-[11.5rem] md:w-[24rem]">{variant.item_name}</h2>
+                        <h2 className="uppercase font-overpass text-sm mobile:text-base md:text-xl">Rp. {variant.sell_price.toLocaleString()}</h2>
                       </div>
                     </div>
                   ))}
               </>
             ) : (
-              <p className="uppercase font-overpass font-bold text-xl]">{error ? `Login failed: ${error}` : 'No products found in this category'}</p>
+              <p className="uppercase font-overpass font-bold text-xl">{error ? `Login failed: ${error}` : 'No products found in this category'}</p>
             )}
           </div>
         </div>
