ccf368b4b514e6f40631eecd6036157e9f64fce9
mazharidesky <muhammad.210170230@mhs.unimal.ac.id>
2025-01-02 00:27:56 +0700

    fix: overflow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import './Navbar.css';
-import Sidebar from './Sidebar';
-import Cart from './Cart';
-import { useCart } from './CartContext';
-import { toast } from 'react-toastify';
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Cookies from "js-cookie";
+import "./Navbar.css";
+import Sidebar from "./Sidebar";
+import Cart from "./Cart";
+import { useCart } from "./CartContext";
+import { toast } from "react-toastify";
 
 const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -35,16 +35,16 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
   const handleLogout = async () => {
     try {
       await axios.delete(`${backendUrl}/logout`);
-      Cookies.remove('accessToken');
-      sessionStorage.removeItem('DetailUser');
+      Cookies.remove("accessToken");
+      sessionStorage.removeItem("DetailUser");
       setIsLoggedIn(false);
       setUserDropdownOpen(false);
-      navigate('/login');
+      navigate("/login");
       window.location.reload();
     } catch (error) {
       console.error(
-        toast.error('Gagal melakukan logout', {
-          position: 'top-right',
+        toast.error("Gagal melakukan logout", {
+          position: "top-right",
           autoClose: 1500,
           hideProgressBar: false,
           closeOnClick: true,
@@ -52,7 +52,8 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
           draggable: true,
           progress: undefined,
           limit: 1,
-          className: 'font-garamond font-bold text-[#333333] px-4 py-2 sm:px-6 sm:py-3 sm:rounded-lg',
+          className:
+            "font-garamond font-bold text-[#333333] px-4 py-2 sm:px-6 sm:py-3 sm:rounded-lg",
         })
       );
     }
@@ -66,9 +67,9 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
         setScrolling(false);
       }
     };
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -78,9 +79,9 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
         setUserDropdownOpen(false);
       }
     };
-    window.addEventListener('click', handleClickOutside);
+    window.addEventListener("click", handleClickOutside);
     return () => {
-      window.removeEventListener('click', handleClickOutside);
+      window.removeEventListener("click", handleClickOutside);
     };
   }, []);
 
@@ -89,41 +90,92 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
   }, [location]);
 
   useEffect(() => {
-    setCartItemCount(cartItems.reduce((total, item) => total + item.quantity, 0));
+    setCartItemCount(
+      cartItems.reduce((total, item) => total + item.quantity, 0)
+    );
   }, [cartItems]);
 
-  const navbarColor = location.pathname === '/' ? (scrolling ? 'bg-white shadow-md' : 'bg-transparent') : 'bg-white shadow-md';
-  const svgColor = scrolling || location.pathname !== '/' ? '#333333' : '#FFFFFF';
-  const textColor = scrolling || location.pathname !== '/' ? 'text-[#333333]' : 'text-white';
+  const navbarColor =
+    location.pathname === "/"
+      ? scrolling
+        ? "bg-white shadow-md"
+        : "bg-transparent"
+      : "bg-white shadow-md";
+  const svgColor =
+    scrolling || location.pathname !== "/" ? "#333333" : "#FFFFFF";
+  const textColor =
+    scrolling || location.pathname !== "/" ? "text-[#333333]" : "text-white";
 
   return (
     <>
-      <nav className={`${navbarColor} fixed top-0 left-0 right-0 w-full z-[999] transition-colors duration-300`}>
+      <nav
+        className={`${navbarColor} fixed top-0 left-0 right-0 w-full z-[999] transition-colors duration-300 overflow-x-hidden`}
+      >
         <div className="max-w-[115rem] py-5 mx-auto px-5 md:px-2 flex justify-between items-center">
           {/* Hamburger Button */}
-          <Link to="#" className="menu-bars transition-colors duration-300" onClick={showSidebar}>
-            <svg className="w-7 h-7 md:w-8 md:h-8" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M5 11.6667H35" stroke={svgColor} strokeWidth="2" strokeLinecap="round" />
-              <path d="M5 20H35" stroke={svgColor} strokeWidth="2" strokeLinecap="round" />
-              <path d="M5 28.3333H35" stroke={svgColor} strokeWidth="2" strokeLinecap="round" />
+          <Link
+            to="#"
+            className="menu-bars transition-colors duration-300"
+            onClick={showSidebar}
+          >
+            <svg
+              className="w-7 h-7 md:w-8 md:h-8"
+              viewBox="0 0 40 40"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5 11.6667H35"
+                stroke={svgColor}
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+              <path
+                d="M5 20H35"
+                stroke={svgColor}
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+              <path
+                d="M5 28.3333H35"
+                stroke={svgColor}
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
             </svg>
           </Link>
           <div className="logo">
-            <Link to="/" className={`font-garamond font-semibold text-xl md:text-3xl tracking-[10px] md:tracking-[18px] ${textColor}`}>
+            <Link
+              to="/"
+              className={`font-garamond font-semibold text-xl md:text-3xl tracking-[10px] md:tracking-[18px] ${textColor}`}
+            >
               SIXSTREET
             </Link>
           </div>
           <div className="flex gap-x-6 relative">
             {/* Users */}
             <div ref={dropdownRef} className="hidden md:block md:relative">
-              <button onClick={toggleUserDropdown} className="flex flex-col items-center">
-                <svg className="w-7 h-7 md:w-8 md:h-8" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" stroke={svgColor}>
+              <button
+                onClick={toggleUserDropdown}
+                className="flex flex-col items-center"
+              >
+                <svg
+                  className="w-7 h-7 md:w-8 md:h-8"
+                  viewBox="0 0 40 40"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke={svgColor}
+                >
                   <path
                     d="M20.0001 20C24.6025 20 28.3334 16.269 28.3334 11.6667C28.3334 7.0643 24.6025 3.33334 20.0001 3.33334C15.3977 3.33334 11.6667 7.0643 11.6667 11.6667C11.6667 16.269 15.3977 20 20.0001 20Z"
                     strokeWidth="2"
                     strokeLinecap="round"
                   />
-                  <path d="M34.3167 36.6667C34.3167 30.2167 27.9 25 20 25C12.1 25 5.68335 30.2167 5.68335 36.6667" strokeWidth="2" strokeLinecap="round" />
+                  <path
+                    d="M34.3167 36.6667C34.3167 30.2167 27.9 25 20 25C12.1 25 5.68335 30.2167 5.68335 36.6667"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                  />
                 </svg>
               </button>
               {userDropdownOpen && (
@@ -131,18 +183,30 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
                   <div className="py-1">
                     {isLoggedIn ? (
                       <>
-                        <Link to={`/profile/${userId}`} className="block px-4 py-2 text-[#333333] font-medium font-overpass text-center">
+                        <Link
+                          to={`/profile/${userId}`}
+                          className="block px-4 py-2 text-[#333333] font-medium font-overpass text-center"
+                        >
                           Profile
                         </Link>
-                        <Link to={'/order-history'} className="block px-4 py-2 text-[#333333] font-medium font-overpass text-center">
+                        <Link
+                          to={"/order-history"}
+                          className="block px-4 py-2 text-[#333333] font-medium font-overpass text-center"
+                        >
                           Order History
                         </Link>
-                        <button onClick={handleLogout} className="block w-full px-4 py-2 text-[#333333] font-medium font-overpass text-center">
+                        <button
+                          onClick={handleLogout}
+                          className="block w-full px-4 py-2 text-[#333333] font-medium font-overpass text-center"
+                        >
                           Logout
                         </button>
                       </>
                     ) : (
-                      <Link to="/login" className="block w-full px-4 py-2 text-[#333333] font-medium font-overpass text-center">
+                      <Link
+                        to="/login"
+                        className="block w-full px-4 py-2 text-[#333333] font-medium font-overpass text-center"
+                      >
                         Login
                       </Link>
                     )}
@@ -153,7 +217,12 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
             {/* End of Users */}
             {/* Cart */}
             <button onClick={showCart} className="relative">
-              <svg className="w-7 h-7 md:w-8 md:h-8" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                className="w-7 h-7 md:w-8 md:h-8"
+                viewBox="0 0 40 40"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path
                   d="M12.5 12.7833V11.1667C12.5 7.41668 15.5167 3.73335 19.2667 3.38335C23.7333 2.95001 27.5 6.46668 27.5 10.85V13.15"
                   stroke={svgColor}
@@ -170,16 +239,45 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn, userId }) => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
-                <path d="M25.8259 20H25.8409" stroke={svgColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M14.1574 20H14.1724" stroke={svgColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path
+                  d="M25.8259 20H25.8409"
+                  stroke={svgColor}
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M14.1574 20H14.1724"
+                  stroke={svgColor}
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
               </svg>
-              {cartItemCount > 0 && <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">{cartItemCount}</span>}
+              {cartItemCount > 0 && (
+                <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                  {cartItemCount}
+                </span>
+              )}
             </button>
           </div>
         </div>
       </nav>
-      <Sidebar sidebarOpen={sidebarOpen} showSidebar={showSidebar} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} userId={userId} />
-      <Cart cartOpen={cartOpen} showCart={showCart} showSidebar={showSidebar} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} userId={userId} />
+      <Sidebar
+        sidebarOpen={sidebarOpen}
+        showSidebar={showSidebar}
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+        userId={userId}
+      />
+      <Cart
+        cartOpen={cartOpen}
+        showCart={showCart}
+        showSidebar={showSidebar}
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+        userId={userId}
+      />
     </>
   );
 };
